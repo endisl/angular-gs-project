2071031ac76ff4b694f3d7130af22cb3806a94a4
endisl <49719707+endisl@users.noreply.github.com>
2021-08-05 21:45:33 -0300

    Add files via upload

---
+++ products/frontend/src/app/components/product/product-update/product-update.component.ts
@@ -0,0 +1,43 @@
+import { ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Product } from "../product.model";
+import { ProductService } from "../product.service";
+
+@Component({
+  selector: "app-product-update",
+  templateUrl: "./product-update.component.html",
+  styleUrls: ["./product-update.component.css"],
+})
+export class ProductUpdateComponent implements OnInit {
+  product: Product = {
+    id: 0,
+    name: "",
+    price: 0,
+  };
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.productService.readById(`${id}`).subscribe((product) => {
+      //Tive que interpolar `${id}` para funcionar |200721
+      this.product = product;
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage("Produto atualizado com sucesso!");
+      this.router.navigate(["/products"]);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(["/products"]);
+  }
+}
